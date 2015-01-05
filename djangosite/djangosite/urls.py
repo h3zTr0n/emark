eb1b050e0b6fa572c2d056ddf640cc7c06a7c6f5
OldGuo <young.guo@gmail.com>
2015-01-04 16:18:19 -0800

    fully integrated into django
    pages all in django now!

--- djangosite/djangosite/urls.py
+++ djangosite/djangosite/urls.py
@@ -6,7 +6,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'djangosite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$','djangosite.views.home_out',name="home_out"),
+    url(r'^$','djangosite.views.home',name="home"),
+    url(r'^browseCategory','djangosite.views.browseCategory',name="browseCategory"),
+    url(r'^browseTag','djangosite.views.browseTag',name="browseTag"),
+    url(r'^itemListing','djangosite.views.itemListing',name="itemListing"),
+    url(r'^search','djangosite.views.search',name="search"),
+    url(r'^shoppingCart','djangosite.views.shoppingCart',name="shoppingCart"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^acc/', include('accountstuff.urls'))
 )
--- djangosite/djangosite/views.py
+++ djangosite/djangosite/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-def home_out(request):
-	 return render_to_response('HomepageOut.html', context_instance=RequestContext(request))
+def home(request):
+	 return render_to_response('homepage.html', context_instance=RequestContext(request))
+def browseCategory(request):	 
+	return render_to_response('browseCategory.html', context_instance=RequestContext(request))
+def browseTag(request):	 
+	return render_to_response('browseTag.html', context_instance=RequestContext(request))
+def itemListing(request):	 
+	return render_to_response('itemListing.html', context_instance=RequestContext(request))
+def search(request):	 
+	return render_to_response('search.html', context_instance=RequestContext(request))
+def shoppingCart(request):	 
+	return render_to_response('shoppingcart.html', context_instance=RequestContext(request))
