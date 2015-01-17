9c7313226d08bda82e15d9266ad69e024b96ec87
William Hsu <will.s.hsu@gmail.com>
2015-01-17 01:37:03 -0800

    finished with api request for messages

--- djangosite/messagestuff/views.py
+++ djangosite/messagestuff/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from messagestuff.models import Message
 from django.contrib.auth.models import User
+import json
 
 # Create your views here.
 #SERVER
@@ -27,4 +28,33 @@ def sendMessage(request):
 
 def viewMessages(request):
 	#recent/numberofmessages/whichpage, all/specificuser
-	return HttpResponse("TODO")
+	howmany = 10
+	page = 0
+	kindof = "to"
+	target = request.user.username
+	if "custom" in request.POST:
+		howmany = request.POST["length"]
+		page = request.POST["page"]
+		kindof = request.POST["specification"] #"from" or "to"
+		target = request.POST["target"]
+
+	target = User.objects.filter(username=target)
+	if (len(target) == 0):
+		return HttpResponse("User does not exist.")
+
+	messages = None
+	if (kindof == "to"):
+		messages = Message.objects.filter(recipient=target).order_by('timestamp')[(page*howmany):(page*howmany)+howmany]
+	elif (kindof == "from"): #oldest first all over! not "-timestamp"
+		messages = Message.objects.filter(sender=target).order_by('timestamp')[(page*howmany):(page*howmany)+howmany]
+	
+	msgobj = []
+	for msg in messages:
+		msgobj.append({
+			"sender": msg.sender.username,
+			"recipient": msg.recipient.username,
+			"timestamp": msg.timestamp.strftime("%B %d, %Y %I:%M:%S %p"),
+			"body": msg.body,
+		})
+
+	return HttpResponse(json.dumps(msgobj))
