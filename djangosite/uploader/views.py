96626780af2066019de1b4f5c81a12fdd2f81365
OldGuo <young.guo@gmail.com>
2015-01-21 15:00:12 -0800

    basic user connection with uploads

--- djangosite/uploader/views.py
+++ djangosite/uploader/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from uploader.models import UploadForm,Upload
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 def home(request):
 	if(request.method=="POST"):
 		img = UploadForm(request.POST,request.FILES)
+		img.user=request.user
 		if img.is_valid():
 			img.save()
 			return HttpResponseRedirect(reverse('imageupload'))
