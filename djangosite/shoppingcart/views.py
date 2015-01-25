b66b5ce817a0d8d2e0fd0498067843a76c5fb40d
Harrding <harrisond2003@gmail.com>
2015-01-24 20:02:14 -0800

    Signed-off-by: Harrding <harrisond2003@gmail.com>

--- djangosite/shoppingcart/views.py
+++ djangosite/shoppingcart/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from shoppingcart.models import ShoppingCartItem
 from itemstuff.models import Item
 from django.template import RequestContext, loader
+import random
 
 # Create your views here.
 def displayCart(request):
@@ -15,13 +16,17 @@ def displayCart(request):
 	template = loader.get_template('DshoppingCart.html')
 	return HttpResponse(template.render(RequestContext(request, context)))
 def addItemToUser(request, itemid, quantity):
-	newItem = ShoppingCartItem(user = request.user, item = Item.objects.filter(itemid = itemid)[0], quantity = quantity, uniqueid = "sci" + Item.objects.filter(itemid = itemid)[0].title.replace(" ", "").lower() +  str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)))
+	if len(ShoppingCartItem.objects.filter(user = request.user, item = Item.objects.filter(itemid = itemid)[0])) == 0:
+		newItem = ShoppingCartItem(user = request.user, item = Item.objects.filter(itemid = itemid)[0], quantity = quantity, uniqueid = "sci" + Item.objects.filter(itemid = itemid)[0].title.replace(" ", "").lower() +  str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9)))
+	else :
+		newItem = ShoppingCartItem.objects.filter(user = request.user, item = Item.objects.filter(itemid = itemid)[0])[0]
+		newItem.quantity += quantity
 	newItem.save()
 	return HttpResponse("Success!!!!!!!!!")
 def sumCartPrices(request, cartlist):
 	sum = 0.0
 	for cartitem in cartlist:
-		sum += cartitem.item.price
+		sum += cartitem.item.price * cartitem.quantity
 	return sum
 def removeItem(request, scitemid):
 	removedItem = ShoppingCartItem.objects.filter(uniqueid = scitemid)
