8dac21190a34598739793e06dd7fb1f67f9c716b
William Hsu <will.s.hsu@gmail.com>
2015-01-24 18:38:08 -0800

    why isn't this working uhh

--- djangosite/itemstuff/migrations/0008_item_picture.py
+++ djangosite/itemstuff/migrations/0008_item_picture.py
@@ -2,21 +2,19 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('uploader', '0001_initial'),
+        ('itemstuff', '0007_item_itemid'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='upload',
-            name='user',
-            field=models.ForeignKey(null=True, to=settings.AUTH_USER_MODEL),
+            model_name='item',
+            name='picture',
+            field=models.FileField(null=True, blank=True, upload_to='items/'),
             preserve_default=True,
         ),
     ]
