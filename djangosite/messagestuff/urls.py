0dc3a050c7ca0d9164c1a6967c0abb3976c9f07e
William Hsu <will.s.hsu@gmail.com>
2015-01-25 12:44:49 -0800

    initial messages

--- djangosite/messagestuff/urls.py
+++ djangosite/messagestuff/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from messagestuff import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.main, name='main'),
-	url(r'^/(?P<username>[A-Za-z0-9]+)/$', views.mainWithUser, name='mainWithUser'),
+	#url(r'^$', views.main, name='main'),
+	url(r'^(?P<username>[A-Za-z0-9]*)$', views.main, name='mainWithUser'),
 	url(r'^info/$', views.allInfo, name='indexAll'),
 	url(r'^pSendMessage/$', views.sendMessage, name='sendMessage'),
 	url(r'^pViewMessages/$', views.viewMessages, name='viewMessages'),
