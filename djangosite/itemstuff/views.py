d7920c3ea2ad98b5906e164ed4b2849860eadfc2
William Hsu <will.s.hsu@gmail.com>
2015-01-27 18:17:25 -0800

    most search stuff

--- djangosite/itemstuff/views.py
+++ djangosite/itemstuff/views.py
@@ -12,7 +12,7 @@ def browseCategory(request, category):
 	#categorypath = request.path.split("/")
 	#category = categorypath[len(categorypath)-1]
 	categories = {
-		"jewelery": 1,
+		"jewelry": 1,
 		"pottery": 2,
 		"sewingweaving": 3,
 		"clothing": 4,
@@ -102,10 +102,44 @@ def comparePrice(a, b):
 def compareTime(a,b):
 	return (a.time - b.time).total_seconds()
 
+def searchnew(request):
+	categories = ["jewelry","pottery","sewingweaving","clothing","art"]
+	#
+	terms = request.GET["q"].split(' ')
+	sortby = "relevancy"
+	if "sort" in request.GET:
+		sortby = request.GET['sort']
+	cata = [True, True, True, True, True]
+	if (len(request.GET) > 1):
+		cata = ["cjewelry" in request.GET, "cpottery" in request.GET, "csewingweaving" in request.GET, "cclothing" in request.GET, "cart" in request.GET]
+	minPrice
+	maxPrice
+
 def search(request):
+	categories = [
+		"jewelry",
+		"pottery",
+		"sewingweaving",
+		"clothing",
+		"art",
+	]
+
 	items = []
 	terms = request.GET["q"].split(' ')
 	sortby = "relevancy"
+	cata = {}
+	if (len(request.GET) > 1):
+		cata["jewelry"] = "cjewelry" in request.GET
+		cata["pottery"] = "cpottery" in request.GET
+		cata["sewingweaving"] = "csewingweaving" in request.GET
+		cata["clothing"] = "cclothing" in request.GET
+		cata["art"] = "cart" in request.GET
+	else:
+		cata["jewelry"] = True
+		cata["pottery"] = True
+		cata["sewingweaving"] = True
+		cata["clothing"] = True
+		cata["art"] = True
 	if "sort" in request.GET:
 		sortby = request.GET['sort'] 
 	for term in terms:
@@ -121,7 +155,6 @@ def search(request):
 		for item in Item.objects.filter(tags__icontains=term):
 			if item not in items:
 				items.append(item)
-
 	def compareRelevancy(a,b):
 		acount = bcount = 0
 		for term in terms:
@@ -154,11 +187,27 @@ def search(request):
 	if sortby == "relevancy":
 		items = sorted(items, key=cmp_to_key(compareRelevancy))
 		items.reverse()
+	items = list(items)
+	for item in items:
+		if (cata[categories[item.category-1]] == False):
+			items.remove(item)
 	context = {
 		"search": request.GET["q"],
 		"items": items,	
 		"sortby": sortby,
+		"cata": cata,
 	}
+	if ("minPrice" in request.GET and request.GET["minPrice"]):
+		context["minPrice"] = float(request.GET["minPrice"])
+		for item in items:
+			if (item.price < float(request.GET["minPrice"])):
+				items.remove(item)
+	if ("maxPrice" in request.GET and request.GET["maxPrice"]):
+		context["maxPrice"] = float(request.GET["maxPrice"])
+		for item in items:
+			if (item.price > float(request.GET["maxPrice"])):
+				items.remove(item)
+	
 	if (request.user.is_authenticated()):
 		context["user"] = request.user
 		context["userinfo"] = UserInfo.objects.filter(user=request.user)[0]
