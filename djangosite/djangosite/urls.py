5ba2c9bbbf9d34214fd447d894244c4607eb9b75
Young <young.guo@gmail.com>
2015-02-09 21:33:38 -0800

    Lots of ui stuff for the checkout page

--- djangosite/djangosite/urls.py
+++ djangosite/djangosite/urls.py
@@ -10,7 +10,6 @@ urlpatterns = patterns('',
 
     url(r'^$','djangosite.views.home',name="home"),
     url(r'^about/$','djangosite.views.about',name="about"),
-    url(r'^error/', 'djangosite.views.error', name="error"),
     url(r'^admin/', include(admin.site.urls)),
 
     #url(r'^browseCategory','djangosite.views.browseCategory',name="browseCategory"),
