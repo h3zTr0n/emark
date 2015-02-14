ab9a55497196a0cb93b195305e96a57809d5db3b
William Hsu <will.s.hsu@gmail.com>
2015-02-13 16:04:27 -0800

    empty item fixes

--- djangosite/djangosite/views.py
+++ djangosite/djangosite/views.py
@@ -28,19 +28,23 @@ def home(request):
 	userinfos = sorted(userinfos, key=cmp_to_key(compareFollowers))
 	userinfos.reverse()
 	featureduserinfos=[]
-	for i in range(0,4):
-		featureduserinfos.append(userinfos[i])
+	featureduserinfos=userinfos[:4]
+	#for i in range(0,4):
+	#	featureduserinfos.append(userinfos[i])
 
 	items = Item.objects.all()
-	randInts3 = random.sample(range(len(items)),3)
 	featureditems = []
-	for rand in randInts3:
-		featureditems.append(items[rand])
-		
-	randInts6 = random.sample(range(len(items)),6)
 	featureditems2 = []
-	for rand in randInts6:
-		featureditems2.append(items[rand])
+
+	if(len(items) >= 3):
+		randInts3 = random.sample(range(len(items)),3)
+		for rand in randInts3:
+			featureditems.append(items[rand])
+
+	if(len(items) >= 6):
+		randInts6 = random.sample(range(len(items)),6)
+		for rand in randInts6:
+			featureditems2.append(items[rand])
 
 	context["featureduserinfos"] = featureduserinfos
 	context["featureditems"] = featureditems
