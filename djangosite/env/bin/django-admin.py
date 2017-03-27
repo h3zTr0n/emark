80cf08f3d8f49ece5afd790487d7ae3f6ff3c2b1
h3zTr0n <alison.mukoma@yahoo.com>
2017-03-27 04:17:57 +0200

    house keeping of files

---
+++ djangosite/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bugtraq/Documents/eMark/test/MVFBLA_E-Business_2014_2015/djangosite/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
